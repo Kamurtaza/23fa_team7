0c1e917540c502937f9252f8e2f8dc5e3fcfb204
SkylarKorn <skorn21103@gmail.com>
2023-09-28 10:37:57 -0400

    Updated FXML files to match user requirements for login and registering.

---
+++ Software_Project/src/application/MainControl.java
@@ -0,0 +1,22 @@
+package application;
+
+public class MainControl {
+	
+	// Swap scene to register page.
+	public void registerAccount() {
+		
+	}
+	
+	// Swap scene to main page.
+	public void visitorLogin() {
+		
+	}
+	
+	// Checks DATA STORAGE HERE for correct username and password.
+	// Pass - Swap scene to main page.
+	// Fail - Create error label.
+	public void userLogin(String username, String password) {
+		
+	}
+	
+}
--- Software_Project/src/module-info.java
+++ Software_Project/src/module-info.java
@@ -1,5 +1,7 @@
 module Software_Project {
 	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.base;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
