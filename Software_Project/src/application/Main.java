de5ee7875a38455e2e739011cabed57fbc88c726
SkylarKorn <skorn21103@gmail.com>
2023-10-12 10:43:04 -0400

    Created a "ButtonHandlers" class
    For the time being, the ButtonHandlers class will handle all button
    actions. Might seperate out later on for each FXML file having its own
    handler class.

--- Software_Project/src/application/Main.java
+++ Software_Project/src/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("Build.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
--- Software_Project/src/module-info.java
+++ Software_Project/src/module-info.java
@@ -2,6 +2,7 @@ module Software_Project {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.base;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
