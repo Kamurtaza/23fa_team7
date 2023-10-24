c3ec10d1b1729ca6a632e4b48f44899be0a48ae8
Jacob Carlstrom <jacobcarlstrom8@gmail.com>
2023-10-24 09:49:42 -0400

    New Objects
    Created Category and Group classes with unit tests for each class.

---
+++ Software_Project/src/application/Group.java
@@ -0,0 +1,35 @@
+package application;
+
+public class Group {
+	private String title;
+	private Category category;
+	
+	public Group(String name, Category category) {
+		this.title = name;
+		this.category = category;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public Category getCategory() {
+		return category;
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if(o instanceof Group) {
+			Group g = (Group)o;
+			if(this.title.equals(g.title)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	@Override
+	public String toString() {
+		return title;
+	}
+}
---
+++ Software_Project/src/application/GroupTest.java
@@ -0,0 +1,47 @@
+package application;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GroupTest {
+
+	@Test
+	void testEquals_True() {
+		Group g = createGroup();
+		Category c = new Category("Science");
+		Group g2 = new Group("Calculus I", c);
+		assertEquals(g,g2);
+	}
+	
+	@Test
+	void testEquals_False() {
+		Group g = createGroup();
+		Category c = new Category("Science");
+		Group g2 = new Group("Biology", c);
+		assertNotEquals(g,g2);
+	}
+	
+	@Test
+	void testEquals_notGroup() {
+		Group g = createGroup();
+		int x = 2;
+		assertNotEquals(g,x);
+	}
+	
+	@Test
+	void testToString() {
+		Group g = createGroup();
+		String expected = "Calculus I";
+		assertEquals(expected, g.toString());
+	}
+	
+	/*
+	 * HELPER METHOD
+	 */
+	
+	public Group createGroup() {
+		Category c = new Category("Mathematics");
+		return new Group("Calculus I", c);
+	}
+}
